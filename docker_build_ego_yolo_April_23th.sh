27dd5c71dcde7db456d19643e29d114e2db59278
allenthreee <59171742+allenthreee@users.noreply.github.com>
2023-04-22 21:05:03 +0800

    Add files via upload

---
+++ docker_build_ego_yolo_April_23th.sh
@@ -0,0 +1,4 @@
+docker pull allenthreee/icuas_simulation_ego_yolo:3rd_April_23th
+
+echo "allenthreee/icuas_simulation_ego_yolo:3rd_April_23th pulled"
+echo "Now you can run ./docker_run_ego_yolo_April_23th.sh to run and test:)"
